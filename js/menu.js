4fd269a6ec45456c62bbe9d7ae0a25a923073133
Andrew220708 <andrew.asmogilov@gmail.com>
2022-09-05 22:13:13 +0300

    Updat

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,14 @@
 (() => {
   const refs = {
-    openMenulBtn: document.querySelector("[data-menu-open]"),
-    closeMenulBtn: document.querySelector("[data-menu-close]"),
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
     menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
   };
-
   refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenulBtn.addEventListener("click", toggleMenu);
-
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
   function toggleMenu() {
     refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
   }
 })();
